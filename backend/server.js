3fca02f0fb17302a123f3eb149ef3eb58ca5961a
Himanshu <himanshukhileree@gmail.com>
2024-10-08 17:19:52 +0000

    auth backend done

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require("express")
 const mongoose = require('mongoose')
 const quizRoutes = require('./routes/quizes')
+const userRoutes = require('./routes/user')
 const cors = require('cors')
 
 const app = express();
@@ -16,6 +17,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/quizes', quizRoutes);
+app.use('/api/user', userRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(
